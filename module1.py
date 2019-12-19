a70e58e1f36507168fa351485023962b79411557
舒波 郭 <2420019491@qq.com>
2019-12-19 15:45:15 +0800

    。。。

--- module1.py
+++ module1.py
@@ -7,7 +7,7 @@ func = np.poly1d(np.array([1,0,0]).astype(float)) #array里的数字从右往左
 x = np.linspace(-10,10,10000)#在-10到10之间取点，取得越多越精确 
 y = func(x)#把x的值放入多项式计算后赋给y 
 plt.plot(x, y) #调用plot函数
-plt.xlabel('x') #创建x轴
-plt.ylabel('y(x)') #创建y轴
+plt.xlabel('x') #创建x轴轴标
+plt.ylabel('y(x)') #创建y轴轴标
 plt.show()#最后用show函数显示函数图像
 
--- 一个简单的多次函数绘图器.py
+++ 一个简单的多次函数绘图器.py
@@ -9,7 +9,22 @@ func=np.poly1d(np.array(a).astype(float))
 x=np.linspace(-100,100,1000)
 y=func(x)
 
-plt.plot(x,y)
-plt.xlabel('x')
-plt.ylabel('y(x)')
+plt.plot(x,y,'r')
+plt.xlim((-5,5))
+plt.ylim((-5,5))#划定显示的范围
+#plt.xlabel('x')
+#plt.ylabel('y(x)')#显示x，y的轴标
+
+#new_ticks=np.linspace(-1,2,5)
+#print(new_ticks)
+#plt.xticks(new_ticks)
+#plt.yticks([-5,-4,2,3,4],
+#           ['red','pink','red','r','w'])#通过一一对应替换掉相应的数标
+ax=plt.gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')#将上框和右框颜色设置为无色（使两条边框消失）
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')#将底边框设置为x轴，将左边框设置为y轴
+ax.spines['bottom'].set_position(('data',0))
+ax.spines['left'].set_position(('data',0))#将x，y两条轴移到0的位置上
 plt.show()
